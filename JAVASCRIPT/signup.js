334a881a516206bfcc13d76d8ef1593b4db46da3
malyn08 <latigarmalyn08@gmail.com>
2025-01-24 14:50:22 +0800

    Update signup.js

--- JAVASCRIPT/signup.js
+++ JAVASCRIPT/signup.js
@@ -3,7 +3,6 @@ import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebase
 import { doc, setDoc } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
 
 document.getElementById('registerBtn').addEventListener('click', async (e) => {
-    e.preventDefault();
 
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
